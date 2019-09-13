89cade463210e2e026bef67fdb7048326406eef7
sifat009 <bhacker150@gmail.com>
2019-09-13 08:11:43 +0600

    added default user location

--- app.js
+++ app.js
@@ -3,38 +3,26 @@ const express = require('express');
 const app = express();
 const geoip = require('geoip-lite');
 
-app.get('/', (req, res) => {
-    let userIP  = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    userIP = userIP.substr(0, 7) === '::ffff:' ? userIP.substr(7) : userIP;
-    const userLocation = geoip.lookup('119.148.8.82');
-    res.send(userIP)
-})
-
-
-
 app.get('/api/weather', (req, res) => {
+    let userLocation;
     if(req.query.location) {
-        getWeather(req.query.location, (error, data)=> {
-            if(error) {
-                return res.send(error);
-            } else {
-                return res.json(data);
-            }
-        })
+        userLocation = req.query.location;
     } else {
-        res.json({error: 'location is missing '})
+        const userIP = req.ip.substr(0, 7) === '::ffff:' ? req.ip.substr(7) : req.ip;
+        if(userIP === '127.0.0.1') {
+            userLocation = 'Amazon';
+        } else {
+            userLocation = geoip.lookup(userIP).city;
+        }
     }
+    getWeather(userLocation, (error, data)=> {
+        if(error) {
+            return res.send(error);
+        } else {
+            return res.json(data);
+        }
+    })
 })
-
-// const place = process.argv[2];
-
-// getWeather(place, (error, res) => {
-//     if(error) {
-//         console.log(error);
-//     } else {
-//         console.log(res);
-//     }
-// })
 
 
 
