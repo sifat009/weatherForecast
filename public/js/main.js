7f917d7cdc44971f1d7e00e96782c04e425f7cc0
sifat009 <sifathaque6@gmail.com>
2019-09-19 17:36:01 +0600

    added final style and just remain to deploy to heroku

--- public/js/main.js
+++ public/js/main.js
@@ -2,19 +2,30 @@ const form = document.querySelector('form');
 const input = document.querySelector('input[type="text"]');
 const temperatureDiv = document.querySelector('.temperature');
 const locationDiv = document.querySelector('.location');
+const detailsDiv = document.querySelector('.details');
 const error = document.querySelector('.error');
 
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
+    temperatureDiv.innerHTML = '';
+    error.innerText = '';
+    locationDiv.innerText = 'Observing weather... 🕵🏻‍♂️';
+    detailsDiv.innerText = '';
+
     const location = input.value;
     getLocation(location)
         .then((data) => {
+            console.log(data)
             if(data.error) {
+                locationDiv.innerText = '';
+                detailsDiv.innerText = '';
                 error.innerText = data.error;
             } else {
+                const details = `Summary: ${data.summary} and ${data.precipProbability}% possibility to Rain`;
                 locationDiv.innerText = `${data.location.toUpperCase()}`;
                 temperatureDiv.innerHTML = ` ${data.temperature} &#8451;`;
+                detailsDiv.innerText = details;
             }
         })
         .catch((error) => {
